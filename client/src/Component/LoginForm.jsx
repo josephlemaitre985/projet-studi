321c0ab62641d8b3b8c6a435e32eb2706ee43369
Joseph <josephlemaitre985@gmail.com>
2023-07-20 17:48:19 +0200

    back

--- client/src/Component/LoginForm.jsx
+++ client/src/Component/LoginForm.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import config from '../admin/config.js';
 import './LoginForm.css';
+import config from '../admin/config.js';
 
-const LoginForm = ({ handleLogin, handleLogout }) => {
+const LoginForm = ({ handleLogin, handleLogout }) => { // Ajout de handleLogout ici
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -18,46 +19,42 @@ const LoginForm = ({ handleLogin, handleLogout }) => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
 
-    axios
-      .post('/api/employees/login', { email, password })
-      .then((response) => {
-        const { success, role } = response.data;
-
-        if (success) {
-          handleLogin(role);
-          navigate('/admin');
-        } else {
-          setErrorMessage('Adresse e-mail ou mot de passe incorrect.');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        setErrorMessage('Une erreur s\'est produite.');
-      });
+    if (email === config.adminEmail && password === config.adminPassword) {
+      handleLogin('admin');
+      navigate('/admin');
+    } else if (email === config.employeeEmail && password === config.employeePassword) {
+      handleLogin('employee');
+      navigate('/admin'); 
+    } else {
+      setErrorMessage('Adresse e-mail ou mot de passe incorrect.');
+    }
   };
 
   return (
     <div className="form div">
-      <h1 className="title">Connexion admin</h1>
+      <h1 className='title'>Connexion admin</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <div>
-            <label className="label">Email:</label>
-            <input className="input" type="email" value={email} onChange={handleEmailChange} placeholder="Votre adresse mail" />
+            <label className='label'>Email:</label>
+            <input className='input' type="email" value={email} onChange={handleEmailChange} placeholder='Votre adresse mail' />
           </div>
           <div>
-            <label className="label">Mot de passe:</label>
-            <input className="input" type="password" value={password} onChange={handlePasswordChange} placeholder="Votre mot de passe" />
+            <label className='label'>Mot de passe:</label>
+            <input className='input' type="password" value={password} onChange={handlePasswordChange} placeholder='Votre mot de passe' />
           </div>
-          <button className="button" type="submit">Se connecter</button>
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button className='button' type="submit">Se connecter</button>
+          {errorMessage && <p className='error-message'>{errorMessage}</p>}
         </div>
       </form>
-      <img src="https://i0.wp.com/my-barn.com/wp-content/uploads/2023/05/logo-garage-1.png?resize=1200%2C720&ssl=1" alt="Logo du garage" className="logo" />
+      <img src='https://i0.wp.com/my-barn.com/wp-content/uploads/2023/05/logo-garage-1.png?resize=1200%2C720&ssl=1' alt="Logo du garage" className='logo' />
       <Link to="/">
-        <button className="homepagebutton" onClick={handleLogout}>
+        <button 
+          className='homepagebutton' 
+          onClick={handleLogout} // Ajout de handleLogout ici
+        >
           Retour à la page d'accueil
         </button>
       </Link>
