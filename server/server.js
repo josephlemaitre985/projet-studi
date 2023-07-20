270654bddbd6ce953fca3e78ed750a90349ef20c
Joseph <josephlemaitre985@gmail.com>
2023-07-20 15:47:47 +0200

    pb api

--- server/server.js
+++ server/server.js
@@ -23,14 +23,16 @@ app.use(cors());
 
 
 
-app.use(express.static(path.join(__dirname,'build')))
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'build','index.html'))
-});
+
 
 app.use('/api/cars', carRoutes);
 app.use('/api/openinghours', openinghoursRoutes); 
 app.use('/api/employees', employeesRoutes);
+
+app.use(express.static(path.join(__dirname,'build')))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname,'build','index.html'))
+});
 
 
 sequelize
